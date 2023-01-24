15b358fb51ea737413e95242206b8f7cebe265cf
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2023-01-24 11:52:47 -0600

    Project init

--- src/Main/PensumIngenieria.java
+++
@@ -1,8 +0,0 @@
-package Main;
-import BackEnd.Pensum;
-public class PensumIngenieria {
-	public static void main(String[] args) {
-		Pensum p = new Pensum();
-		p.readFilePensum("CursosECYS.csv");
-	}
-}
